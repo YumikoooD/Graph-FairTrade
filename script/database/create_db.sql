912ce8660462b4aab22dc2cebb9e6ab357f31e34
Mikael Sourati <mikaelsourati@gmail.com>
2023-06-09 17:10:03 +0200

    [ADD] script

---
+++ script/database/create_db.sql
@@ -0,0 +1,27 @@
+
+
+create table 
+    votes (
+        id text primary key
+        transaction text
+        block_number bigint
+        block_timestamp timestamp with time zone default timezone ('utc'::text, now()) not null
+        project_id text
+        application_index int
+        round_address text
+        voter text
+        grant_address text
+        token text
+        amount bigint
+        amount_usd money
+    )
+
+create table
+    contributions (
+        id text primary key,
+        project_id text,
+        wallet_id text,
+        amount_contributed_usd money,
+        created_at timestamp with time zone default timezone ('utc'::text, now()) not null,
+        updated_at timestamp with time zone default timezone ('utc'::text, now()) not null
+    );
